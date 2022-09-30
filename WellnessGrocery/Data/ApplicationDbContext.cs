505dfa80257b6b99aaf0c06f3821359f6af79727
singh-v6 <singhv6@mail.uc.edu>
2022-09-29 23:10:49 -0400

    Add project files.

---
+++ WellnessGrocery/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WellnessGrocery.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
